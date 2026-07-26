deedde3bb68e4fbb13b030b0ab878e8c0d33115b
Ranit Ghosh <ranitghosh068@gmail.com>
2026-07-26 11:53:07 +0530

    Time: 10 ms (7.40%) | Memory: 43.4 MB (19.18%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,37 @@
+
+  class Solution {
+    public int longestPalindrome(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+
+        int ans = 0;
+        boolean odd = false;
+
+        // Calculate answer
+        for (int count : map.values()) {
+            if (count % 2 == 0) {
+                ans += count;
+            } else {
+                ans += count - 1;
+                odd = true;
+            }
+        }
+
+        if (odd) {
+            ans++;
+        }
+
+        return ans;
+    }
+}
